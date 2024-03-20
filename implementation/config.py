d85e125a0a731a923c412e73843f9b36df4ca616
primarypit <2895123212@qq.com>
2024-03-20 14:53:09 +0800

    Update

--- implementation/config.py
+++ implementation/config.py
@@ -37,7 +37,7 @@ class ProgramsDatabaseConfig:
           sampling temperature.
     """
     functions_per_prompt: int = 2
-    num_islands: int = 5
+    num_islands: int = 6
     reset_period: int = 4 * 60 * 60
     cluster_sampling_temperature_init: float = 0.1
     cluster_sampling_temperature_period: int = 30_000
